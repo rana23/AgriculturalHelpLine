8094f43bf47a35044f5514bf2767afad65b43fa5
anweshapaul <anwesha110206@gmail.com>
2014-08-24 11:08:34 +0600

    Create ffood.java

---
+++ ffood.java
@@ -0,0 +1,16 @@
+package kucse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.agriculturalhelpline.R;
+
+public class ffood extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ffood);
+	}
+	
+}
