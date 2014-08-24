777ce1ea8d4e22e395ec0715833fb70175c2f952
rana23 <mehedi.rahman@live.com>
2014-08-23 21:12:16 -0700

    Create fquailbird.java

---
+++ fquailbird.java
@@ -0,0 +1,18 @@
+package kucse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.agriculturalhelpline.R;
+
+public class fquailbird extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fquailbird);
+	}
+	
+}
+
+
