712708fb115675017c93b719839445823356a388
rana23 <mehedi.rahman@live.com>
2014-08-23 21:09:16 -0700

    Create fpigeons.java

---
+++ fpigeons.java
@@ -0,0 +1,16 @@
+package kucse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.agriculturalhelpline.R;
+
+public class fpigeons extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fpigeons);
+	}
+	
+}
