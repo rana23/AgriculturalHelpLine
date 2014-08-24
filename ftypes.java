2812390a9dcc53fc20c6060982f6d073c94e5dcd
anweshapaul <anwesha110206@gmail.com>
2014-08-24 11:13:12 +0600

    Create ftypes.java

---
+++ ftypes.java
@@ -0,0 +1,16 @@
+package kucse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.agriculturalhelpline.R;
+
+public class ftypes extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ftypes);
+	}
+	
+}
